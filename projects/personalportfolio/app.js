ac89cebfb675b0db74cd37698e3eab97b07204d6
Miriam Williams <mlwill07@gmail.com>
2017-02-27 14:50:34 -0700

    moved project to new repository

--- projects/personalportfolio/app.js
+++ projects/personalportfolio/app.js
@@ -1,9 +1,37 @@
-angular.module('myApp', ['ngRoute', 'ui.bootstrap'])
+$(document).ready(function () {
 
-.config(['$routePovider', function($routeProvider){
+    //    $('a[href*="#"]:not([href="#"])').click(function () {
+    //        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    //            var target = $(this.hash);
+    //            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //            if (target.length) {
+    //                $('html, body').animate({
+    //                    scrollTop: target.offset().top
+    //                }, 1000);
+    //                return false;
+    //            }
+    //        }
+    //    });
     
-    $routeProvider
-        .when('/', {
-        
-    })
-}])
+    
+
+    $("a").on('click', function (event) {
+
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+
+                window.location.hash = hash;
+            });
+        }
+    });
+
+})
