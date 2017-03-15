c702bd9f287e187e9da8fea3958ac9421b9bf4bd
Miriam Williams <mlwill07@gmail.com>
2017-03-15 08:58:22 -0600

    updates to personal portfolio

--- projects/personalportfolio/app.js
+++ projects/personalportfolio/app.js
@@ -1,20 +1,5 @@
 $(document).ready(function () {
-
-    //    $('a[href*="#"]:not([href="#"])').click(function () {
-    //        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    //            var target = $(this.hash);
-    //            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    //            if (target.length) {
-    //                $('html, body').animate({
-    //                    scrollTop: target.offset().top
-    //                }, 1000);
-    //                return false;
-    //            }
-    //        }
-    //    });
     
-    
-
     $("a").on('click', function (event) {
 
 
@@ -33,5 +18,12 @@ $(document).ready(function () {
             });
         }
     });
+    
+    $(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+
 
 })
